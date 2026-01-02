7889ba312d40a009c02b58694f284e872a4bcbb7
zeekhoft <dltposiedon@gmail.com>
2026-01-02 22:45:11 +0800

    added services

--- components/features-1.tsx
+++ components/features-1.tsx
@@ -1,66 +1,86 @@
 import { Card, CardContent, CardHeader } from '@/components/ui/card'
-import { Settings2, Sparkles, Zap } from 'lucide-react'
+import { LucideIcon, Settings2, Sparkles, Zap } from 'lucide-react'
 import { ReactNode } from 'react'
+
+interface Contents {
+    title: string,
+    context: string,
+    icon: LucideIcon
+}
+
+
+function ServicesCard({ title, context, icon: Icon }: Contents) {
+    return (
+        <Card className="group shadow-zinc-950/5">
+            <CardHeader className="pb-3">
+                <CardDecorator>
+                    <Icon
+                        className="size-6"
+                        aria-hidden
+                    />
+                </CardDecorator>
+
+                <h3 className="mt-6 font-medium">{title}</h3>
+            </CardHeader>
+
+            <CardContent>
+                <p className="text-sm">{context}</p>
+            </CardContent>
+        </Card>
+    );
+}
+
+
 
 export default function Features() {
     return (
         <section className="bg-zinc-50 py-16 md:py-32 dark:bg-transparent">
-            <div className="@container mx-auto max-w-5xl px-6">
+            <div className="@container mx-auto max-w-7xl px-6">
                 <div className="text-center">
                     <h2 className="text-balance text-4xl font-semibold lg:text-5xl">Built to cover your needs</h2>
                     <p className="mt-4">Libero sapiente aliquam quibusdam aspernatur, praesentium iusto repellendus.</p>
                 </div>
                 <div className="@min-4xl:max-w-full @min-4xl:grid-cols-3 mx-auto mt-8 grid max-w-sm gap-6 *:text-center md:mt-16">
-                    <Card className="group shadow-zinc-950/5">
-                        <CardHeader className="pb-3">
-                            <CardDecorator>
-                                <Zap
-                                    className="size-6"
-                                    aria-hidden
-                                />
-                            </CardDecorator>
 
-                            <h3 className="mt-6 font-medium">Customizable</h3>
-                        </CardHeader>
+                    <ServicesCard
+                        icon={Zap}
+                        title='Lead Generation'
+                        context='Multi-Channel Outreach. A mix of email, calls, and social
+                            platforms that converts. We provide targeted strategies
+                            to attract qualified leads, fill your sales pipeline, and drive
+                            consistent business growth.
+                            ' />
 
-                        <CardContent>
-                            <p className="text-sm">Extensive customization options, allowing you to tailor every aspect to meet your specific needs.</p>
-                        </CardContent>
-                    </Card>
+                    <ServicesCard
+                        icon={Zap}
+                        title='Social Media Management'
+                        context='Providing full-service management of your social
+                        platforms—creating content, scheduling posts, and
+                        engaging with your audience' />
 
-                    <Card className="group shadow-zinc-950/5">
-                        <CardHeader className="pb-3">
-                            <CardDecorator>
-                                <Settings2
-                                    className="size-6"
-                                    aria-hidden
-                                />
-                            </CardDecorator>
 
-                            <h3 className="mt-6 font-medium">You have full control</h3>
-                        </CardHeader>
+                    <ServicesCard
+                        icon={Zap}
+                        title='Content Creation'
+                        context='Creating high-quality visual and written content that tells
+                        your brand story and supports marketing goals.' />
+                    <ServicesCard
+                        icon={Zap}
+                        title='Lead Profiling'
+                        context='We deliver lead lists customized to match your ideal customer profile.
+' />
+                    <ServicesCard
+                        icon={Zap}
+                        title='Executive Assistance'
+                        context='We provide reliable administrative support to help executives stay
+                focused, organized, and efficient.' />
+                    <ServicesCard
+                        icon={Zap}
+                        title='Customer Service Rep'
+                        context='We provide support that enhances client satisfaction, addresses their
+                    needs, and builds strong, long-term relationships.' />
 
-                        <CardContent>
-                            <p className="mt-3 text-sm">From design elements to functionality, you have complete control to create a unique and personalized experience.</p>
-                        </CardContent>
-                    </Card>
 
-                    <Card className="group shadow-zinc-950/5">
-                        <CardHeader className="pb-3">
-                            <CardDecorator>
-                                <Sparkles
-                                    className="size-6"
-                                    aria-hidden
-                                />
-                            </CardDecorator>
-
-                            <h3 className="mt-6 font-medium">Powered By AI</h3>
-                        </CardHeader>
-
-                        <CardContent>
-                            <p className="mt-3 text-sm">Elements to functionality, you have complete control to create a unique experience.</p>
-                        </CardContent>
-                    </Card>
                 </div>
             </div>
         </section>
--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -15,15 +15,15 @@ export default function HeroSection() {
                     <div className="pb-24 pt-12 md:pb-32 lg:pb-56 lg:pt-44">
                         <div className="relative mx-auto flex max-w-6xl flex-col px-6 lg:block">
                             <div className="mx-auto max-w-lg text-center lg:ml-0 lg:w-1/2 lg:text-left">
-                                <h1 className="mt-8 max-w-2xl text-balance text-5xl font-medium md:text-6xl lg:mt-16 xl:text-7xl">Ship 10x Faster with NS</h1>
-                                <p className="mt-8 max-w-2xl text-pretty text-lg">Highly customizable components for building modern websites and applications that look and feel the way you mean it.</p>
+                                <h1 className="mt-8 max-w-2xl text-balance text-5xl font-medium md:text-7xl lg:mt-16 xl:text-9xl uppercase">Project Based</h1>
+                                <p className="mt-8 max-w-2xl text-pretty text-lg">Discover our service tiers designed to fuel growth, enhance operations, and position your business for long-term success.</p>
 
                                 <div className="mt-12 flex flex-col items-center justify-center gap-2 sm:flex-row lg:justify-start">
                                     <Button
                                         asChild
                                         size="lg"
                                         className="px-5 text-base">
-                                        <Link href="#link">
+                                        <Link href="#Contact">
                                             <span className="text-nowrap">Start Building</span>
                                         </Link>
                                     </Button>
